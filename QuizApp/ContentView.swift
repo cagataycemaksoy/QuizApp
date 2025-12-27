78f71beb969d0bf9b6af589da30994eae5310e55
cagataycemaksoy <cemaksoy7890@gmail.com>
2025-12-27 18:25:57 +0100

    animation and rotation added

--- QuizApp/ContentView.swift
+++ QuizApp/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
   @State private var count = 0
   @State private var xOffset: CGFloat = .zero
+  @State private var rotationDegrees: CGFloat = .zero
   
   var body: some View {
     GeometryReader { geo in
@@ -21,15 +22,18 @@ struct ContentView: View {
           .fill(Color(red: 0.95, green: 0.9, blue: 0.8))
           .frame(width: (geo.size.width/3) * 2, height: geo.size.height / 2)
           .offset(x: xOffset)
+          .rotationEffect(.init(degrees: rotationDegrees))
           .gesture(
             DragGesture()
               .onChanged { value in
                 xOffset = value.translation.width
+                rotationDegrees = value.translation.width / 15
               }
               .onEnded { _ in
                 count += xOffset < 0 ? -1 : 1
                 withAnimation(.smooth(duration: 0.5, extraBounce: 0.2)) {
                   xOffset = .zero
+                  rotationDegrees = .zero
                 }
                 
               }
