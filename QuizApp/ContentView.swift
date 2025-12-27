ff4499d71a8601901a4edc0ab25aa1093f6eab8c
cagataycemaksoy <cemaksoy7890@gmail.com>
2025-12-27 17:40:25 +0100

    initial UI

--- QuizApp/ContentView.swift
+++ QuizApp/ContentView.swift
@@ -8,15 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+  @State private var count = 0
+  
+  var body: some View {
+    GeometryReader { geo in
+      VStack {
+        Text("Count: \(count)")
+          .font(.title.bold())
+        
+        RoundedRectangle(cornerRadius: 30)
+          .fill(Color(red: 0.5, green: 0.7, blue: 0.85))
+          .frame(width: (geo.size.width/3) * 2, height: geo.size.height / 2)
+        
+      }
+      .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
+  }
 }
 
 #Preview {
