38390b9bcd8f8c9553de40460232aace33b852f8
cagataycemaksoy <cemaksoy7890@gmail.com>
2025-12-27 17:09:38 +0100

    Initial Commit

---
+++ QuizApp/QuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppApp.swift
+//  QuizApp
+//
+//  Created by Cem Aksoy on 27.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
