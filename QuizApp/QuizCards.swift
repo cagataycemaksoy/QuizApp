8c741e541024adf793cb1bb8b0b4768b98d64594
cagataycemaksoy <cemaksoy7890@gmail.com>
2025-12-28 23:54:04 +0100

    updated UI

--- QuizApp/QuizCards.swift
+++ QuizApp/QuizCards.swift
@@ -15,34 +15,39 @@ struct QuizCards<Content: View>: View {
   @State private var rotationDegrees: CGFloat = .zero
   
     var body: some View {
-      GeometryReader { geo in
-        VStack {
-          Text("Count: \(count)")
-            .font(.title.bold())
-          
-          RoundedRectangle(cornerRadius: 30)
-            .fill(Color(red: 0.95, green: 0.9, blue: 0.8))
-            .frame(width: (geo.size.width/3) * 2, height: geo.size.height / 2)
-            .offset(x: xOffset)
-            .rotationEffect(.init(degrees: rotationDegrees))
-            .gesture(
-              DragGesture()
-                .onChanged { value in
-                  xOffset = value.translation.width
-                  rotationDegrees = value.translation.width / 15
-                }
-                .onEnded { _ in
-                  count += xOffset < 0 ? -1 : 1
-                  withAnimation(.smooth(duration: 0.5, extraBounce: 0.2)) {
-                    xOffset = .zero
-                    rotationDegrees = .zero
-                  }
-                  
-                }
-              
-            )
+      Group(subviews: content){ collection in
+        GeometryReader { geo in
+          VStack {
+            Text("Count: \(count)")
+              .font(.title.bold())
+            
+            ForEach(collection) { view in
+              view
+                .frame(width: (geo.size.width/3) * 2, height: (geo.size.height/2) * 1.3)
+                .background(
+                  RoundedRectangle(cornerRadius: 30)
+                    .fill(Color(red: 0.98, green: 0.93, blue: 0.83))
+                  )
+                .offset(x: xOffset)
+                .rotationEffect(.init(degrees: rotationDegrees))
+                .gesture (
+                  DragGesture()
+                    .onChanged { value in
+                      xOffset = value.translation.width
+                      rotationDegrees = value.translation.width / 15
+                    }
+                    .onEnded { _ in
+                      count += xOffset < 0 ? -1 : 1
+                      withAnimation(.smooth(duration: 0.5, extraBounce: 0.2)) {
+                        xOffset = .zero
+                        rotationDegrees = .zero
+                      }
+                    }
+                )
+            }
+          }
+          .frame(maxWidth: .infinity, maxHeight: .infinity)
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
       }
     }
 }
