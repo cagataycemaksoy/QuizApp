97659c5ccfa2928da157848a665b9019a21d8804
cagataycemaksoy <cemaksoy7890@gmail.com>
2025-12-31 20:53:18 +0100

    implemented project

--- QuizApp/QuizCards.swift
+++ QuizApp/QuizCards.swift
@@ -14,47 +14,50 @@ struct QuizCards<Content: View>: View {
   @State private var xOffset: CGFloat = .zero
   @State private var rotationDegrees: CGFloat = .zero
   
-    var body: some View {
-      Group(subviews: content){ collection in
-        GeometryReader { geo in
-          VStack {
-            Text("Count: \(count)")
-              .font(.title.bold())
-            
-            ZStack{
-              ForEach(collection) { view in
-                let indexZ = collection.count - (collection.firstIndex(where: {$0.id == view.id}) ?? 0)
-                
-                view
-                  .frame(width: (geo.size.width/3) * 2, height: (geo.size.height/2) * 1.3)
-                  .background(
-                    RoundedRectangle(cornerRadius: 30)
-                      .fill(Color(red: 0.98, green: 0.93, blue: 0.83))
-                    )
-                  .offset(x: xOffset)
-                  .rotationEffect(.init(degrees: rotationDegrees))
-                  .gesture (
-                    DragGesture()
-                      .onChanged { value in
-                        xOffset = value.translation.width
-                        rotationDegrees = value.translation.width / 15
+  var body: some View {
+    
+    GeometryReader { geo in
+      VStack {
+        Text("Count: \(count)")
+          .font(.title.bold())
+        
+        Group(subviews: content){ collection in
+          ZStack{
+            ForEach(collection) { view in
+              let indexZ = collection.count - (collection.firstIndex(where: {$0.id == view.id}) ?? 0)
+              
+              view
+                .frame(width: (geo.size.width/3) * 2, height: (geo.size.height/2) * 1.3)
+                .background(
+                  RoundedRectangle(cornerRadius: 30)
+                    .fill(Color(red: 0.98, green: 0.93, blue: 0.83))
+                )
+                .offset(x: xOffset)
+                .rotationEffect(.init(degrees: rotationDegrees))
+                .gesture (
+                  DragGesture()
+                    .onChanged { value in
+                      xOffset = value.translation.width
+                      rotationDegrees = value.translation.width / 15
+                    }
+                    .onEnded { _ in
+                      count += xOffset < 0 ? -1 : 1
+                      withAnimation(.smooth(duration: 0.5, extraBounce: 0.2)) {
+                        xOffset = .zero
+                        rotationDegrees = .zero
                       }
-                      .onEnded { _ in
-                        count += xOffset < 0 ? -1 : 1
-                        withAnimation(.smooth(duration: 0.5, extraBounce: 0.2)) {
-                          xOffset = .zero
-                          rotationDegrees = .zero
-                        }
-                      }
-                  )
-                  .zIndex(Double(indexZ))
-              }
+                    }
+                )
+                .zIndex(Double(indexZ))
             }
           }
-          .frame(maxWidth: .infinity, maxHeight: .infinity)
         }
+        
       }
+      .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
+    
+  }
   
 }
 
